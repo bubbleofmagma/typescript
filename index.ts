684ee655e447048f3dae453b9b5cb90f6e912a07
skMagma <87656578+skMagma@users.noreply.github.com>
2023-01-25 11:51:44 +0000

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,19 @@
+
+
+console.log("Hi")
+
+var name = "ben";
+
+console.log(name)
+
+uppername = name.toUpperCase()
+lowername = name.toLowerCase()
+
+console.log(uppername)
+console.log(lowername)
+
+var num = 25.123456
+
+var truncated = num.trunc()
+
+console.log(truncated)
